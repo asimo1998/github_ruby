3a7233fec94ee2b719915d362d7abdb97da10b3d
asimo98 <letungbach89@gmail.com>
2020-09-03 15:58:02 +0700

    add new file

--- index.rb
+++ index.rb
@@ -3,5 +3,12 @@ puts "hello world"
 
 print "nhap vao gia tri  cua x "
 x = gets.chomp.to_i
-
 puts "gia tri cua x la #{x}"
+x = 1
+while x <= 3
+    x+=1
+    puts "#{x}"
+    
+end
+
+puts "day la doan viet them"
